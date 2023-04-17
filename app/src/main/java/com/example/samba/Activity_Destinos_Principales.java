1500f46ae06865dc412f9ea88df3788c8cc2218d
ivall <ivallejo@elpuig.xeill.net>
2023-04-17 15:21:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/samba/Activity_Destinos_Principales.java
@@ -0,0 +1,14 @@
+package com.example.samba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Destinos_Principales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destinos_principales);
+    }
+}
