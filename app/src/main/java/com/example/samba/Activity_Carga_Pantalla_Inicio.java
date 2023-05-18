c5917b6f6a2f8ea6f9ca60280fc9a4213df5f8a0
Samuel Veiga <sveiga@elpuig.xeill.net>
2023-05-18 20:21:10 +0200

    SplashActivity

---
+++ app/src/main/java/com/example/samba/Activity_Carga_Pantalla_Inicio.java
@@ -0,0 +1,23 @@
+package com.example.samba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Activity_Carga_Pantalla_Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carga_pantalla_inicio);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Activity_Carga_Pantalla_Inicio.this, Activity_Acceso.class));
+            }
+        }, 3000 );
+    }
+}
